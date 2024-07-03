72f578da5f7a1400cf8b77b0862456762ec4d680
matt <matt@fiddaman.net>
2024-07-03 00:19:39 +0000

    add pullRequestReview handlers for changes requested, approved and needs more approvals

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,7 +1,9 @@
 import { Probot } from 'probot';
 
 import PullRequest from './pullRequest.js';
+import PullRequestReview from './pullRequestReview.js';
 
 export default (app: Probot) => {
   PullRequest(app);
+  PullRequestReview(app);
 };
---
+++ src/handlers/pullRequestReview.ts
@@ -0,0 +1,12 @@
+import { Probot } from 'probot';
+
+import PullRequest from '../classes/PullRequest.js';
+
+export default (app: Probot) => {
+  app.on(['pull_request_review'], async context => {
+    const pr = new PullRequest(context);
+    const reviewStatus = await pr.getReviewStatus();
+
+    await pr.addLabel(reviewStatus);
+  });
+};
