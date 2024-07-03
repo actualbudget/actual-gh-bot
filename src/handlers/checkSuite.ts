56e0a28adb5331e372af203aac149d7c0370b670
matt <matt@fiddaman.net>
2024-07-03 00:19:59 +0000

    add checkSuite handlers for failing CI

---
+++ src/handlers/checkSuite.ts
@@ -0,0 +1,43 @@
+import { Probot } from 'probot';
+
+import PullRequest from '../classes/PullRequest.js';
+
+export default (app: Probot) => {
+  app.on(['check_suite.completed'], async context => {
+    const commitSha = context.payload.check_suite.head_sha;
+    const prNum = context.payload.check_suite.pull_requests[0].number;
+
+    const pr = await PullRequest.getFromNumber(context, prNum);
+
+    if (pr.wip) return;
+
+    const suites = (
+      await context.octokit.checks.listSuitesForRef({
+        owner: pr.owner,
+        repo: pr.repo,
+        ref: commitSha,
+      })
+    ).data.check_suites;
+
+    const runs = [];
+    for (const suite of suites) {
+      const run = (
+        await context.octokit.checks.listForSuite({
+          owner: pr.owner,
+          repo: pr.repo,
+          check_suite_id: suite.id,
+        })
+      ).data.check_runs;
+
+      runs.push(...run);
+    }
+
+    const failed = runs.some(r => r.conclusion === 'failure');
+
+    if (failed) {
+      await pr.addLabel('failingCI');
+    } else {
+      await pr.addLabel('readyForReview');
+    }
+  });
+};
--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,9 +1,11 @@
 import { Probot } from 'probot';
 
+import CheckSuite from './checkSuite.js';
 import PullRequest from './pullRequest.js';
 import PullRequestReview from './pullRequestReview.js';
 
 export default (app: Probot) => {
+  CheckSuite(app);
   PullRequest(app);
   PullRequestReview(app);
 };
