b26a1a15b7e050df82136d7b125fd570a7ca1658
matt <matt@fiddaman.net>
2024-08-05 23:01:04 +0000

    change approved label to only consider reviews from users with push access

--- src/classes/PullRequest.ts
+++ src/classes/PullRequest.ts
@@ -124,8 +124,17 @@ export default class PullRequest {
 
     if (reviews.length < 1) return 'readyForReview';
 
-    const latestReviewsObj: { [key: number]: { state: string; time: number } } =
-      {};
+    const pushUsers = (
+      await this.octokit.repos.listCollaborators({
+        owner: this.owner,
+        repo: this.repo,
+        permission: 'push',
+      })
+    ).data.map(u => u.id);
+
+    const latestReviewsObj: {
+      [key: number]: { state: string; time: number; user: number };
+    } = {};
 
     for (const r of reviews) {
       if (!r?.user?.id || !r.submitted_at) continue;
@@ -133,31 +142,32 @@ export default class PullRequest {
       const user = r.user.id;
       const time = new Date(r.submitted_at).getTime();
 
-      if (!latestReviewsObj[user]) {
-        latestReviewsObj[user] = {
-          state: r.state,
-          time,
-        };
+      const review = {
+        state: r.state,
+        time,
+        user,
+      };
 
+      if (!latestReviewsObj[user]) {
+        latestReviewsObj[user] = review;
         continue;
       }
 
       if (latestReviewsObj[user].time < time) {
-        latestReviewsObj[user] = {
-          state: r.state,
-          time,
-        };
+        latestReviewsObj[user] = review;
       }
     }
 
     const latestReviews = Object.values(latestReviewsObj);
+
+    if (!latestReviews.length) return 'readyForReview';
 
     if (latestReviews.filter(r => r.state === 'CHANGES_REQUESTED').length > 0) {
       return 'changesRequested';
     }
 
     const approvingReviews = latestReviews.filter(
-      r => r.state === 'APPROVED',
+      r => r.state === 'APPROVED' && pushUsers.includes(r.user),
     ).length;
     const requiredReviews = await this.getRequiredReviews();
 
@@ -165,6 +175,10 @@ export default class PullRequest {
       return 'needsMoreApprovals';
     }
 
-    return 'approved';
+    if (approvingReviews > 0) {
+      return 'approved';
+    }
+
+    return 'readyForReview';
   }
 }
